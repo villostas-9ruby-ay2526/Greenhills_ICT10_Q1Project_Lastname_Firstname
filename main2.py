726a2d35a7c8ca5f5235160cdaa0f4f5b6b5d985
VILLOSTAS DV <villostas.dv@obmontessori.edu.ph>
2026-09-21 06:57:58 +0800

    Update main2.py
    changed

--- main2.py
+++ main2.py
@@ -3,12 +3,12 @@ from pyscript import display, document
 
 #This will help calculate for the price
 def get_reciept(e):
-    prod_1 = document.getElementbyId("first_option")
-    prod_2 = document.getElementbyId("second_option")
-    prod_3 = document.getElementbyId("third_option")
+    prod_1 = (int.document.getElementbyId("first_option").value)
+    prod_2 = (int.document.getElementbyId("second_option").value)
+    prod_3 = (int.document.getElementbyId("third_option").value)
     subtotal = prod_1 + prod_2 + prod_3
     tax_rate = subtotal * 0.12
     total = subtotal + tax_rate
 
 
-    display(f'Subtotal: {subtotal}, Tax: {tax_rate},  Total: {total}, target='result')
+    display(f'Subtotal: {subtotal} Tax: {tax_rate}  Total: {total}' target='price')
